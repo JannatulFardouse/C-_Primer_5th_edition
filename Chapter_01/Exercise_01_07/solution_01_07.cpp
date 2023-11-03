1020b5be17746b84c36e88cb9a780b559946e3e0
JannatulFardouse <62265695+JannatulFardouse@users.noreply.github.com>
2023-11-03 08:55:16 +0000

    update exercise_01_07 solution

---
+++ Chapter_01/Exercise_01_07/solution_01_07.cpp
@@ -0,0 +1,17 @@
+/*-- 
+    Exercise 1.7: Compile a program that has incorrectly nested comments.
+ --*/
+ 
+ //Solution by Jannatul Fardouse
+
+ int main(){
+    /*
+        start the first block.
+
+        /* A nested block comment*/
+
+        End of the nested block comment. inside the comment block is not allowed.
+
+    */
+    return;
+ }
