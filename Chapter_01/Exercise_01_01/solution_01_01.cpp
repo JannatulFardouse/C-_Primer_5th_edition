0737d2b6d61f39ac72e6152cdadd470583d1633f
JannatulFardouse <62265695+JannatulFardouse@users.noreply.github.com>
2023-11-03 07:34:40 +0000

    update exercise_01_02 solutions

--- Chapter_01/Exercise_01_01/solution_01_01.cpp
+++ Chapter_01/Exercise_01_01/solution_01_01.cpp
@@ -2,6 +2,7 @@
 Exercise 1.1: Review the documentation for your compiler and determine what file
 naming convention it uses. Compile and run the main program from page 2.
 */
+// Solution by Jannatul Fardouse
 int main(){
 return 0;
 }
