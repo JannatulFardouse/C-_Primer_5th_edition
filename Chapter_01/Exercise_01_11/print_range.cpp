338b13c98b9d63b6bfa38e5052d6c9ccb059a7e4
JannatulFardouse <62265695+JannatulFardouse@users.noreply.github.com>
2023-11-03 12:35:52 +0000

    inpu step  done for exercise_01_11 solution

---
+++ Chapter_01/Exercise_01_11/print_range.cpp
@@ -0,0 +1,21 @@
+/*--
+ Exercise 1.11: Write a program that prompts the user for two integers. Print each
+number in the range specified by those two integers. 
+--*/
+
+/* now we are going to print the range of two integers */
+
+#include <iostream>
+
+int main(){
+
+    int min, max;
+    std :: cout<< " Enter the min integer number or start of range " ;
+    std:: cin>>min;
+    std:: cout << " Enter the max integer number or end of range ";
+    std:: cin>>max;
+
+    //simple valid data entred by the user
+
+    return 0;
+}
