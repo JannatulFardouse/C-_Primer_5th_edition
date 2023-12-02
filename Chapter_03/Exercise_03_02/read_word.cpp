7c5fe35cafc542cfa2cc04b72e34afff29adf8bc
JannatulFardouse <62265695+JannatulFardouse@users.noreply.github.com>
2023-12-02 07:13:58 +0000

    update exercise_03_02 solution

---
+++ Chapter_03/Exercise_03_02/read_word.cpp
@@ -0,0 +1,26 @@
+/*
+    Exercise 3.2: Write a program to read the standard input a line at a time. Modify your
+    program to read a word at a time.
+*/
+
+// Solution by Jannatul Fardouse
+
+// read a word at a time
+
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string input;
+    while(cin >> input)
+    {
+        cout << input << endl;
+    }
+    return 0;
+}
